f5ea3e2cd3b3a3d85ff4b4cf3bf4e566ccc920f3
Karlen95h <karlen.h.1995@gmail.com>
2026-07-11 15:14:21 +0400

    add audio

---
+++ audio.js
@@ -0,0 +1,32 @@
+let data = {
+    title: [
+        "Billie Eilish- everything i wanted",
+        "Debuss Reverie",
+        "Freddie Mercury - Mamma",
+        "Nemra - Nare",
+        "polo-pan-canopee",
+        "Tigran Hamasyan Leaving Paris"
+    ],
+
+    song: [
+        "music/Billie Eilish - everything i wanted.mp3",
+        "music/Debussy Reverie.mp3",
+        "music/Freddie Mercury - Mamma (Bohemian Rhapsody).mp3",
+        "music/Nemra - Nare.mp3",
+        "music/polo-pan-canopee.mp3",
+        "music/Tigran Hamasyan Leaving Paris.mp3"
+    ],
+
+    poster: [
+        "https://i.gifer.com/7d20.gif",
+        "https://media2.giphy.com/media/5xtDarmSceWsGyD4VeE/source.gif",
+        "https://cdn.dribbble.com/users/1242979/screenshots/7099165/music.gif",
+        "https://i.gifer.com/8RWM.gif",
+        "https://media2.giphy.com/media/Y0GyZQpjqafoatTvjB/giphy.gif",
+        "https://cdn.dribbble.com/users/1921422/screenshots/5511883/freddie.gif"
+    ]
+
+
+
+
+}
